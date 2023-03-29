5e6a34afd66786ee90a3ef0f352528d2dd2090c6
santhya2021 <129311397+santhya2021@users.noreply.github.com>
2023-03-29 21:41:01 +0530

    Add files via upload
    ClassRoomCode

---
+++ day1/Mobile.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class Mobile {
+
+	public void sendSMS() {
+		// TODO Auto-generated method stub
+		System.out.println("From oppo");
+	}
+	public void makeCall() {
+		// TODO Auto-generated method stub
+		System.out.println("Calling My Friend");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//creating object
+		Mobile operation=new Mobile();
+		operation.sendSMS();
+		operation.makeCall();
+		
+
+	}
+
+}
